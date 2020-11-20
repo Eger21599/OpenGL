b2c18a071b61e74d18f1b18f5e36f2c556e6c235
Egerous32 <akuliizud@gmail.com>
2020-11-20 13:38:56 +0300

    Translate, rotate and scale

--- EgerOpenGL/main.cpp
+++ EgerOpenGL/main.cpp
@@ -4,33 +4,50 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/mat4x4.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.005f;
+
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 //Vertex shader
-static const char* vShader = "														\n\
-#version 330																		\n\
-																					\n\
-layout(location = 0) in vec3 pos;													\n\
-																					\n\
-																					\n\
-void main()																			\n\
-{																					\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, 0.4 * pos.z, 1.0);					\n\
+static const char* vShader = "																		\n\
+#version 330																						\n\
+																									\n\
+layout(location = 0) in vec3 pos;																	\n\
+																									\n\
+uniform mat4 model;																					\n\
+																									\n\
+void main()																							\n\
+{																									\n\
+	gl_Position = model * vec4(pos, 1.0);															\n\
 }";
 
 //Fragment shader
-static const char* fShader = "														\n\
-#version 330																		\n\
-																					\n\
-out vec4 colour;																	\n\
-																					\n\
-void main()																			\n\
-{																					\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);												\n\
+static const char* fShader = "																		\n\
+#version 330																						\n\
+																									\n\
+out vec4 colour;																					\n\
+																									\n\
+void main()																							\n\
+{																									\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);																\n\
 }";
 
 void CreateTriangle()
@@ -117,7 +134,7 @@ void CompileShaders()
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -176,16 +193,36 @@ int main()
 
 		if (direction)
 		{
-			triMaxOffset += triIncrement;
+			triOffset += triIncrement;
 		}
 		else
 		{
-			triMaxOffset -= triIncrement;
+			triOffset -= triIncrement;
 		}
 
 		if (abs(triOffset) >= triMaxOffset)
 		{
 			direction = !direction;
+		}
+
+		curAngle += 0.1f;
+		if (curAngle >= 360)
+		{
+			curAngle -= 360;
+		}
+
+		if (direction)
+		{
+			curSize += 0.001f;
+		}
+		else
+		{
+			curSize -= 0.001f;
+		}
+
+		if (curSize >= maxSize || curSize <= minSize)
+		{
+			sizeDirection = !sizeDirection;
 		}
 
 		//Clear window
@@ -194,7 +231,12 @@ int main()
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model(1.0f);
+		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(curSize, 0.4f, 1.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
